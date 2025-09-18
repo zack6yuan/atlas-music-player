00cb4a22d5774c56ce7d2dab2e2bd6edb7c833fe
zack6yuan <zack6yuan@gmail.com>
2025-09-18 11:21:00 -0500

    2xl, xl, lg, and md margins complete for the left panel.

--- src/components/CoverArt.jsx
+++ src/components/CoverArt.jsx
@@ -2,7 +2,7 @@ import placeholder from "../assets/placeholder.svg"
 
 export default function CoverArt() {
     return (
-        <div className="cover-art scale-33 2xl:-mt-95 2xl:-ml-66">
+        <div className="cover-art 2xl:scale-33 2xl:-mt-95 2xl:-ml-66 xl:scale-33 xl:-mt-95 xl:-ml-85 lg:scale-33 lg:-mt-94 lg:-ml-100 md:scale-29 md:-mt-88 md:-ml-88">
             <img src={ placeholder } alt="" className="rounded-3xl"/>
         </div>
     )
--- src/components/PlayControls.jsx
+++ src/components/PlayControls.jsx
@@ -5,7 +5,7 @@ import shuffle from "../assets/shuffle.svg";
 
 export default function PlayControls() {
     return (
-        <div className="play-controls flex 2xl:gap-18">
+        <div className="play-controls flex 2xl:gap-18 xl:gap-17 lg:gap-17 md:gap-11">
             <p>1x</p>
             <img src={ previous } alt="previous-icon" class=""/>
             <img src={ play } alt="play-icon" class=""/>
