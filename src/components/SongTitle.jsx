73b48ae879a7b4f8ebebbf8238f46a6dcc09c7f4
zack6yuan <zack6yuan@gmail.com>
2025-09-18 11:45:47 -0500

    all margins are set for the left column.

--- src/components/SongTitle.jsx
+++ src/components/SongTitle.jsx
@@ -3,13 +3,13 @@ import VolumeControls from "./VolumeControls";
 
 export default function SongTItle() {
     return (
-        <div className="song-title 2xl:ml-80 2xl:-mt-130 xl:-mt-130 xl:ml-45 lg:-mt-130 lg:ml-15 md:-mt-135 md:ml-6">
+        <div className="song-title 2xl:ml-80 2xl:-mt-130 xl:-mt-130 xl:ml-45 lg:-mt-130 lg:ml-15 md:-mt-135 md:ml-6 sm:ml-6 sm:-mt-17">
             <h1 class="text-2xl font-bold">Painted in Blue</h1>
             <p class="text-slate-500 font-normal mt-2">Soul Canvas</p>
-            <div className="play 2xl:mt-6 xl:mt-6 xl:ml-3 lg:ml-2 lg:mt-6 md:ml-5 md:mt-5">
+            <div className="play 2xl:mt-6 xl:mt-6 xl:ml-3 lg:ml-2 lg:mt-6 md:ml-5 md:mt-5 sm:mt-5 sm:ml-4">
                 <PlayControls />
             </div>
-            <div className="volume 2xl:mt-6 2xl:-ml-3 xl:mt-6 xl:-ml-3 lg:mt-6 lg:-ml-3 md:mt-5 md:-ml-3">
+            <div className="volume 2xl:mt-6 2xl:-ml-3 xl:mt-6 xl:-ml-3 lg:mt-6 lg:-ml-3 md:mt-5 md:-ml-3 sm:mt-6 sm:-ml-2">
                 <VolumeControls />
             </div>
         </div>
