b7985153b0551cdf59af87d9bc305a5d9dfa8b25
zack6yuan <zack6yuan@gmail.com>
2025-09-19 12:31:35 -0500

    updates.

--- src/components/PlayControls.jsx
+++ src/components/PlayControls.jsx
@@ -6,9 +6,8 @@ import shuffle from "../assets/shuffle.svg";
 export default function PlayControls() {
     return (
         <div className="play-controls flex 2xl:gap-17 xl:gap-17 lg:gap-17 md:gap-11 sm:gap-25">
-            <p className="">1x</p>
             <img src={ previous } alt="previous-icon" className=""/>
-            <img src={ play } alt="play-icon" className=""/>
+            <img src={ play } alt="play-icon" className="border-3 rounded-lg p-4"/>
             <img src={ next } alt="next-icon" className=""/>
             <img src={ shuffle } alt="shuffle-icon" className=""/>
         </div>
