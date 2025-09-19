f005c7fbe6d29832655df5c4885b59e4eaf744f8
zack6yuan <zack6yuan@gmail.com>
2025-09-18 22:15:03 -0500

    updates.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -17,7 +17,7 @@ Tailwind Breakpoints
 
 function App() {
   return (
-    <div className="h-full flex flex-col justify-between p-8 min-h-screen">
+    <div className="h-full flex flex-col justify-between p-8 min-h-scree">
       <MusicPlayer />
       <div className="shadow-md">
         <CoverArt />
--- src/components/VolumeControls.jsx
+++ src/components/VolumeControls.jsx
@@ -3,7 +3,7 @@ import volume from "../assets/volume.svg";
 export default function VolumeControls() {
     return (
         <div className="volume-controls flex">
-            <img src={ volume } alt="volume-icon" className="mx-3"/>
+            <img src={ volume } alt="volume-icon" className="mx-3 "/>
             <input type="range" min="0" max="100" className="accent-slate-600 2xl:w-90 xl:w-90 lg:w-90 md:w-67 sm:w-122"/>
         </div>
     )
