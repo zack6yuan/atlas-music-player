7c8a171c56bb0d210b7d3c9c57dedb0e9ccbe8e1
zack6yuan <zack6yuan@gmail.com>
2025-09-19 12:10:48 -0500

    fixed the margins for the left panel.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -17,7 +17,7 @@ Tailwind Breakpoints
 
 function App() {
   return (
-    <div className="h-full flex flex-col justify-between p-8 min-h-scree">
+    <div className="h-full flex flex-col justify-between p-8 min-h-screen">
       <MusicPlayer />
       <div className="shadow-md">
         <CoverArt />
--- src/components/SongTitle.jsx
+++ src/components/SongTitle.jsx
@@ -3,7 +3,7 @@ import VolumeControls from "./VolumeControls";
 
 export default function SongTItle() {
     return (
-        <div className="song-title 2xl:ml-80 2xl:-mt-130 xl:-mt-110 xl:ml-45 lg:-mt-100 lg:ml-15 md:-mt-105 md:ml-6 sm:ml-6">
+        <div className="song-title 2xl:ml-80 2xl:-mt-95 xl:-mt-95 xl:ml-45 lg:-mt-100 lg:ml-15 md:-mt-88 md:mb-6 md:ml-6 sm:mb-6 sm:ml-6">
             <h1 className="text-2xl font-bold">Painted in Blue</h1>
             <p className="text-slate-500 font-normal mt-2">Soul Canvas</p>
             <div className="play 2xl:mt-6 xl:mt-6 xl:ml-3 lg:ml-2 lg:mt-6 md:ml-5 md:mt-5 sm:mt-5 sm:ml-4">
