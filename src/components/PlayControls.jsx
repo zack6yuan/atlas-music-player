eeb5a65d1322863fc82009fb6cb42e4702dc061b
zack6yuan <zack6yuan@gmail.com>
2025-09-19 12:41:53 -0500

    changed the icon for the speed.

--- src/components/PlayControls.jsx
+++ src/components/PlayControls.jsx
@@ -2,10 +2,12 @@ import previous from "../assets/previous.svg"
 import play from "../assets/play.svg"
 import next from "../assets/next.svg";
 import shuffle from "../assets/shuffle.svg";
+import speed from "../assets/speed.svg";
 
 export default function PlayControls() {
     return (
-        <div className="play-controls flex 2xl:gap-17 xl:gap-17 lg:gap-17 md:gap-11 sm:gap-25">
+        <div className="play-controls flex 2xl:gap-14.5 xl:gap-14.5 lg:gap-14.5 md:gap-8 sm:gap-22">
+            <img src={ speed } alt="speed-icon" />
             <img src={ previous } alt="previous-icon" className=""/>
             <img src={ play } alt="play-icon" className="border-3 rounded-lg p-4"/>
             <img src={ next } alt="next-icon" className=""/>
