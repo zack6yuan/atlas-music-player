97992e48e81181f4df39ac16208e3f77d1e5e353
zack6yuan <zack6yuan@gmail.com>
2025-09-20 13:47:21 -0500

    updates.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,4 +1,19 @@
+import sun from "../assets/sun.svg";
+import moon from "../assets/moon.svg";
+
 export default function Footer() {
   const year = new Date().getFullYear();
-  return <div className="text-center p-8 sm:mt-50">&copy; {year} Atlas School</div>;
+  return (
+    <div className="">
+        <div className="text-center p-8 sm:mt-50">
+            &copy; {year} Atlas School
+        </div>
+        <div className="theme-button text-center mb-10">
+            <button>
+                <img src={ moon } alt="dark-theme-icon" />
+                <img src={ sun } alt="light-theme-icon" />
+            </button>
+        </div>
+    </div>
+  );
 }
